d86fec159b4f51d6bd855964f17dc183147401d4
eeshana08 <104460496+eeshana08@users.noreply.github.com>
2023-10-30 17:58:29 +0000

    Add files via upload

---
+++ detect_scissor.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+import tensorflow as tf
+
+model = tf.keras.models.load_model('keras_model.h5')
+
+video = cv2.VideoCapture(0)
+
+while True:
+
+    check,frame = video.read()
+
+
+
+    img = cv2.resize(frame,(224,224))
+
+
+    test_image = np.array(img, dtype=np.float32)
+    test_image = np.expand_dims(test_image, axis=0)
+
+
+    normalised_image = test_image/255.0
+
+   
+    prediction = model.predict(normalised_image)
+
+    print("Prediction : ", prediction)
+        
+    cv2.imshow("Result",frame)
+            
+    key = cv2.waitKey(1)
+
+    if key == 32:
+        print("Closing")
+        break
+
+video.release()
